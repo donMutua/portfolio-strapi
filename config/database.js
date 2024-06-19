c72a6b548dd0a2ce7319f13db60a7f7fec6ab3d4
don.technologist <don.technologist@gmail.com>
2024-06-19 21:37:17 +0300

    changes on config file

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ export default ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapidb"),
-      user: env("DATABASE_USERNAME", ""),
+      database: env("DATABASE_NAME", "strapidatabase"),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
