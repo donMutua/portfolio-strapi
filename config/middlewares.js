8b0d92b11b7bd8e990a225425e158a09914d9777
don.technologist <don.technologist@gmail.com>
2024-06-21 10:11:46 +0300

    chore: Change to mysql from postgres

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,34 @@
-module.exports = [
+module.exports = ({ env }) => [
+  "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
-  "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  // other plugin configurations ...
   upload: {
     config: {
       provider: "aws-s3",
@@ -8,17 +9,15 @@ module.exports = ({ env }) => ({
           secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
           region: env("AWS_REGION"),
           params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
             Bucket: env("AWS_BUCKET_NAME"),
           },
         },
-      },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-      actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
         },
       },
     },
