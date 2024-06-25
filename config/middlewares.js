2bb77f74f07ac27697953a20793a51dcc1632a94
don.technologist <don.technologist@gmail.com>
2024-06-25 12:32:33 +0300

    return middleware to default

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,7 @@
-module.exports = ({ env }) => [
+module.exports = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
