03d1e86f8884ec29bbec9fb988c0bab75a4fb18e
don.technologist <pakanyizid@gmail.com>
2024-12-05 23:26:11 +0300

    chore: Add midlleware changes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
@@ -8,5 +9,6 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
+    serveAdminPanel: true,
   },
 });
